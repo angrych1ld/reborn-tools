f53f4c6abf174f725589a06f616d26ffdb7a8241
Andrius Bendoraitis <abendoraitis300@gmail.com>
2020-11-27 22:40:24 +0200

    Added rest of rbs without spawn info

--- RaidBoss.cs
+++ RaidBoss.cs
@@ -10,6 +10,8 @@ namespace Reborn_Tools
         public uint ID { get; set; }
         public string Title { get; set; }
         public int Level { get; set; }
+        public bool? Alive { get; set; }
+
 
         public string RebornSummaryAddress { get; set; }
         public string L2PortalLocationAddress { get; set; }
