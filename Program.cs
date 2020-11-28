a1057f8ec78c26a33cbb7bb18a050144ed012ae1
Andrius Bendoraitis <abendoraitis300@gmail.com>
2020-11-28 17:22:16 +0200

    Added a world map

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
-namespace Reborn_Tools
+namespace RebornTools
 {
     public class Program
     {
