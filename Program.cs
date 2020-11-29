25c9530f7e9910dc4459328bfff01d84a16ebffc
Andrius Bendoraitis <abendoraitis300@gmail.com>
2020-11-29 19:41:03 +0200

    Updated map to be full screen, added zooming with scroll wheel

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using MatBlazor;
 
 namespace RebornTools
 {
@@ -19,7 +20,7 @@ namespace RebornTools
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddMatBlazor();
             await builder.Build().RunAsync();
         }
     }
