d2b1a80eacda643dc2a86264db503afa75aa0a8e
Andrius Bendoraitis <abendoraitis300@gmail.com>
2020-12-01 00:09:55 +0200

    Move some data from HTML to json file, added 'Location Source' to raidboss data

--- DataAccess.cs
+++ DataAccess.cs
@@ -43,23 +43,8 @@ namespace RebornTools
 
         public static async Task<List<RaidBoss>> FetchRaidbosses(HttpClient http)
         {
-            Stream dataStream = await http.GetStreamAsync("third-party-pages/l2reborn_npc-data_raids.data");
-
-            HtmlParser parser = new HtmlParser();
-            IHtmlDocument document = parser.ParseDocument(dataStream);
-
-            IEnumerable<IElement> validParents = document.All.Where(e => e.TagName.Equals("DIV") && e.ClassName == "entry-content");
-
-            IEnumerable<IElement> raidAnchors = validParents.SelectMany(p => p.Children.Where(e => e.TagName.Equals("A")
-                && e.ClassName == "raid" && e.HasAttribute("HREF") && e.GetAttribute("HREF").Contains(@"/npc/")));
-
-            return raidAnchors.Select(e => new RaidBoss
-            {
-                ID = uint.Parse(e.GetAttribute("href").Split("/", StringSplitOptions.RemoveEmptyEntries)[1]),
-                Title = e.InnerHtml.Substring(0, e.InnerHtml.IndexOf("(")),
-                Level = int.Parse(e.InnerHtml.Split("(", StringSplitOptions.RemoveEmptyEntries)[1].Split(")", StringSplitOptions.RemoveEmptyEntries)[0]),
-                RebornSummaryAddress = @"https://l2reborn.com" + e.GetAttribute("href")
-            }).ToList();
+            Stream dataStream = await http.GetStreamAsync("data/raidbosses.json");
+            return await JsonSerializer.DeserializeAsync<List<RaidBoss>>(dataStream);
         }
 
         public static async Task<List<RaidbossLocation>> FetchRaidbossLocations(HttpClient http)
--- RaidBoss.cs
+++ RaidBoss.cs
@@ -1,23 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace RebornTools
 {
     public class RaidBoss
     {
+        public enum LocationSourceType { Unknown = 0, GraciaPortal = 1, L2RebornManual = 2}
+
         public uint ID { get; set; }
         public string Title { get; set; }
         public int Level { get; set; }
-        public bool? Alive { get; set; }
         public int CoordinateX { get; set; }
         public int CoordinateY { get; set; }
+        public int CoordinateZ { get; set; }
+        public LocationSourceType LocationSource { get; set; }
 
-
-        public string RebornSummaryAddress { get; set; }
+        [JsonIgnore]
+        public bool? Alive { get; set; }
+        [JsonIgnore]
         public string L2PortalLocationAddress { get; set; }
-
+        [JsonIgnore]
         public int AliveInt => Alive == null ? 2 : (Alive.Value ? 0 : 1);
     }
 }
